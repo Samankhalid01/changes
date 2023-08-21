da7a6db664e161b3343a8b6a3bea4967e2922b20
Saman <samankalid0001@gmail.com>
2023-08-22 00:41:26 +0500

    first commit

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
 
@@ -13,8 +15,23 @@ app.get("/api", (req, res) => {
     res.json({ "users": ["userOne", "userTwo", "userThree"] });
 });
 
+dotenv.config();
+
+export const connection = async () => {
+  try {
+    const conc = await mongoose.connect("paste your db conection url in doublecoats here", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("<--- Connection Establised Successfully --->");
+  } catch (error) {
+    console.log("Error ", error.message);
+  }
+};
+
+connection();
 // ider chala rhi hun  server
 const PORT = 5000;
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+    console.log(`Server started on port ${5000}`);
 });
